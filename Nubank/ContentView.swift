70b0f996c54fe148d306aff25acd3fdb1abe078d
Victor Bozelli Alvarez <vbozelli@paypal.com>
2022-01-19 11:36:49 -0300

    Initializing repository

---
+++ Nubank/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Nubank
+//
+//  Created by Victor Bozelli on 1/16/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Nubank/NubankApp.swift
@@ -0,0 +1,17 @@
+//
+//  NubankApp.swift
+//  Nubank
+//
+//  Created by Victor Bozelli on 1/16/22.
+//
+
+import SwiftUI
+
+@main
+struct NubankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
