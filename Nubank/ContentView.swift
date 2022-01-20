5765559e136a4ce8b628f7afb3c1bb4578b1ebf6
Victor Bozelli Alvarez <vbozelli@paypal.com>
2022-01-20 13:05:47 -0300

    Added the Nubank logo images and implemented the header and the card from the Home screen

--- Nubank/ContentView.swift
+++ Nubank/ContentView.swift
@@ -9,7 +9,53 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        ZStack {
+            
+            Color(.sRGB, red: 0.55, green: 0.06, blue: 0.68, opacity: 1.0).ignoresSafeArea()
+            
+            VStack {
+                
+                VStack {
+                    HStack(alignment: .center) {
+                        
+                        Image(uiImage: UIImage(named: "Nubank")!)
+                        
+                        Text("Victor")
+                        .foregroundColor(Color.white)
+                        .font(.system(size: 18))
+                        .fontWeight(.bold)
+                        .padding(EdgeInsets(top: 0, leading: 8, bottom: 0, trailing: 0))
+                    }
+                    .padding(EdgeInsets(top: 32, leading: 0, bottom: 0, trailing: 0))
+                }
+                
+                Image(systemName: "chevron.down")
+                .foregroundColor(Color.white)
+                .padding(EdgeInsets(top: 8, leading: 0, bottom: 8, trailing: 0))
+                
+                ZStack {
+                    Color.white
+                    
+                    VStack(alignment: .center) {
+                        HStack {
+                            Text("Saldo disponível")
+                            .foregroundColor(Color(.sRGB, red: 0.6, green: 0.6, blue: 0.6, opacity: 1.0))
+                            .font(.system(size: 14))
+                            
+                            Text("R$ 0,00")
+                            .foregroundColor(Color(.sRGB, red: 0.2, green: 0.2, blue: 0.2, opacity: 1.0))
+                            .font(.system(size: 32))
+                        }
+                        .frame(maxWidth: .infinity, alignment: .center)
+                    }
+                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
+                }
+                .frame(maxHeight: 400)
+                .cornerRadius(4)
+                .padding(EdgeInsets(top: 0, leading: 16, bottom: 0, trailing: 16))
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+        }
     }
 }
 
